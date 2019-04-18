87c158a5f552a025513364af6b9bd938ef2505f2
SHOREMORTGAGE\stara <stara@uwm.com>
2019-04-18 00:11:40 -0400

    add controller and views

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
